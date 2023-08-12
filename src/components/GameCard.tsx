ea7c8c8cbb00a5c915322b9215c0a1d163cfc0a6
kareemyasser8 <kareem.yasserr@gmail.com>
2023-08-12 15:44:52 +0200

    Clean up the game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -13,16 +13,16 @@ const GameCard = ({ game }: Props) => {
   return (
     <>
       <Card>
-        <Image src={ getCroppedImageUrl(game.background_image) }></Image>
+        <Image src={getCroppedImageUrl(game.background_image)}></Image>
         <CardBody>
-          <Heading fontSize="2xl">{game.name}</Heading>
-         
-          <HStack justifyContent={"space-between"}>
+          <HStack justifyContent={"space-between"} marginBottom={3}>
             <PlatformIconList
               platforms={game.parent_platforms.map((p) => p.platform)}
             ></PlatformIconList>
-             <CriticScore score={game.metacritic}></CriticScore>
+            <CriticScore score={game.metacritic}></CriticScore>
           </HStack>
+
+          <Heading fontSize="2xl">{game.name}</Heading>
         </CardBody>
       </Card>
     </>
