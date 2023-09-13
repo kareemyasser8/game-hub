93fa584e60e379c5bb1defbf6b4610d8c2ec4522
kareemyasser8 <kareem.yasserr@gmail.com>
2023-09-13 14:05:31 +0200

    Refactor- Simplify game query

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import { Grid, GridItem, Show, HStack, Box } from "@chakra-ui/react"
-import NavBar from "./components/NavBar"
-import GameGrid from "./components/GameGrid"
-import GenreList from "./components/GenreList"
+import { Box, Grid, GridItem, HStack, Show } from "@chakra-ui/react"
 import { useState } from "react"
-import { Genre } from "./Hooks/useGenres"
-import PlatformSelector from "./components/PlatformSelector"
-import { Platform } from "./Hooks/usePlatforms"
-import SortSelector from "./components/SortSelector"
+import GameGrid from "./components/GameGrid"
 import GameHeading from "./components/GameHeading"
+import GenreList from "./components/GenreList"
+import NavBar from "./components/NavBar"
+import PlatformSelector from "./components/PlatformSelector"
+import SortSelector from "./components/SortSelector"
 
 export interface GameQuery {
-  genre: Genre | null
-  platform: Platform | null
+  genreId?: number
+  platformId?: number
   sortOrder: string
   searchText: string
 }
@@ -43,9 +41,9 @@ function App() {
         <Show above="lg">
           <GridItem area="aside" paddingX={5}>
             <GenreList
-              selectedGenre={gameQuery.genre}
+              selectedGenreId={gameQuery.genreId}
               onSelectGenre={(genre) => {
-                setGameQuery({ ...gameQuery, genre })
+                setGameQuery({ ...gameQuery, genreId: genre.id })
               }}
             />
           </GridItem>
@@ -56,9 +54,9 @@ function App() {
             <GameHeading gameQuery={gameQuery} />
             <HStack spacing={5} >
               <PlatformSelector
-                selectedPlatform={gameQuery.platform}
+                selectedPlatformId={gameQuery.platformId}
                 onSelectPlatform={(platform) =>
-                  setGameQuery({ ...gameQuery, platform })
+                  setGameQuery({ ...gameQuery, platformId: platform.id})
                 }
               ></PlatformSelector>
               <SortSelector
